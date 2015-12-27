b4ba691bfcbf28f8fc9fb4f0e7d29d3a1243ad46
jsong88 <jaysong88@gmail.com>
2015-12-27 23:03:47 +0900

    modify restart.sh

--- netview/restart.sh
+++ netview/restart.sh
@@ -1,2 +1,11 @@
-kill -9 `lsof -wni tcp:3333 | awk '{print $2}' | grep -v PID`
+#!/bin/bash
+
+echo "### Kill 3333-listening processes"
+tgtpid=`lsof -wni tcp:3333 | awk '{print $2}' | grep -v PID`
+if [ -n "$tgtpid" ]; then
+        echo "The PIDs to be killed: $tgtpid"
+        kill -9 $tgtpid
+fi
+echo "### Start Netview server"
+sleep 3
 nohup node server.js &
